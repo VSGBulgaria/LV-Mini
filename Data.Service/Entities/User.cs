7e0a8c172813ef0a4e2b2d19f1489996b92e5be1
Simeon Banev <Simeon@VSG.COM>
2017-12-14 11:37:18 +0200

    Initial Project Model

---
+++ Data.Service/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Service.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+    }
+}
