853944cdc27add10bf3012ee762e8d894d5437ab
Yani Pirgov <Yani@VSG.COM>
2017-12-18 16:09:42 +0200

    Fixed Navigation Bar. Sort Of :D :D :D

--- LVMini/wwwroot/js/site.js
+++ LVMini/wwwroot/js/site.js
@@ -1,1 +1,16 @@
 ﻿// Write your JavaScript code.
+function checkForm(form) {
+    re = /^[A-Za-z0-9_]{4,20}$/;
+    rep = /^[A-Za-z0-9]{6,20}$/;
+    if (!re.test(form.usr.value)) {
+        alert("Error: Username must contain only letters, numbers and underscores!");
+        form.usr.focus();
+        return false;
+    }
+
+    if (!rep.test(form.pass.value)) {
+        alert("Error:Password is not valid !");
+        form.pass.focus();
+        return false;
+    }
+}
