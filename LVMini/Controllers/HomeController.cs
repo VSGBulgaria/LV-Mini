543ca80b5980edafa44f916f088bc5845dc7e6ec
Yani Pirgov <Yani@VSG.COM>
2017-12-18 10:58:35 +0200

    Initial Commit

--- LVMini/Controllers/HomeController.cs
+++ LVMini/Controllers/HomeController.cs
@@ -29,6 +29,12 @@ namespace LVMini.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            ViewData["Message"] = "Your Registrtion Page";
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
