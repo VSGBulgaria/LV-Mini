af634bed5a6a2b4889cd8cd286f47afa61408b62
Yani Pirgov <Yani@VSG.COM>
2018-01-11 16:43:21 +0200

    Fixed View Of The Reg Form

--- AuthorizationServer/Quickstart/Account/RegisterViewModel.cs
+++ AuthorizationServer/Quickstart/Account/RegisterViewModel.cs
@@ -5,23 +5,32 @@ namespace AuthorizationServer.Quickstart.Account
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Username")]
         public string Username { get; set; }
 
         [Required]
         [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(50)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [StringLength(50)]
+        [Display(Name = "Last Name")] //This Is A Label
         public string LastName { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 5)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
