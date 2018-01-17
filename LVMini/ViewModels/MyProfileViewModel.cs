145cc20f0a333eae09ac25b6f6802cbdd4e05d16
Yani Pirgov <Yani@VSG.COM>
2018-01-17 14:21:01 +0200

    Fixed Buttons, Added Users Page

--- LVMini/ViewModels/MyProfileViewModel.cs
+++ LVMini/ViewModels/MyProfileViewModel.cs
@@ -8,11 +8,6 @@ namespace LVMini.ViewModels
 {
     public class MyProfileViewModel
     {
-        // [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
-        [DataType(DataType.Text)]
-        [Display(Name = "Username")]
-        public string Username { get; set; }
 
         // [Required]
         [EmailAddress]
@@ -28,17 +23,6 @@ namespace LVMini.ViewModels
         [DataType(DataType.Text)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
-        //[Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-       // [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
     }
