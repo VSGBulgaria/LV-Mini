331146a64bdf6e893ff749d9c24441d374af790d
Yani Pirgov <Yani@VSG.COM>
2018-01-23 15:46:31 +0200

    Get Users Functionality Fixed.JS Code Refactured

--- LVMini/wwwroot/js/site.js
+++ LVMini/wwwroot/js/site.js
@@ -1,31 +1,4 @@
 ﻿// Write your JavaScript code.
-//Functions For The LogIn Form
-
-
-function checkForm(form) {
-    re = /^[A-Za-z0-9_]{4,20}$/;
-    rep = /^[A-Za-z0-9]{6,20}$/;
-    if (!re.test(form.usr.value)) {
-        alert("Error: Username must contain only letters, numbers and underscores!");
-        form.usr.focus();
-        return false;
-    }
-
-    if (!rep.test(form.pass.value)) {
-        alert("Error:Password is not valid !");
-        form.pass.focus();
-        return false;
-    }
-}
-//Function For facebook Login
-
-var finished_rendering = function () {
-    console.log("finished rendering plugins");
-    var facebook = document.getElementById("facebook");
-    facebook.removeAttribute("style");
-    facebook.removeChild(facebook.childNodes[0]);
-}
-//FB.Event.subscribe('xfbml.render', finished_rendering);
 
 // For Map Location
 function initMap() {
@@ -96,6 +69,7 @@ function displayUsernameSign(isAvailable) {
         $(unav_sign).css(cssDisplayKeyWord, cssInlineBlockKeyWord);
     }
 }
+
 //MyProfile Edit Finctions
 $("#btnEdit").click(function () {
     if ($("#editMyProfile").hasClass("hidden"))
@@ -103,10 +77,12 @@ $("#btnEdit").click(function () {
 });
 
 //Get Users Edit
-$("#btnUEdit").click(function () {
-        if ($("#adminEdit").hasClass("hidden"))
-            $("#adminEdit").removeClass("hidden");
- });
+$('.btnUEdit').click(function (ev) {
+    let userIdetity = ev.currentTarget.id;
+    let userIdentityAsJqueryString = '#hidden' + userIdetity;
+    let hiddenUserTemplate = $(userIdentityAsJqueryString);
+    $(hiddenUserTemplate).attr('class', 'col-lg-6');
+});
 //For Get Users Page
     //$(document).ready(function () {
 
